4afa53272142e2c17a82b8cb9cf3fa073fd87b8b
James Park <james.park.victoria@gmail.com>
2018-09-15 18:19:17 -0700

    Crossfade functionality added

--- portfolio/indexScript.js
+++ portfolio/indexScript.js
@@ -1,13 +1,14 @@
 $(function() {
    $(window).scroll(function () {
-      if ($(this).scrollTop() > 400) {
-        $('body').css('background-image', 'url("images/main.jpg")');
-        $('body').css('background-attachment', 'fixed');
-        $('body').css('background-position', '50% 50%');
-        $('body').css('background-size', 'cover');
-      }
-      if ($(this).scrollTop() < 400) {
-        $('body').css('background', 'rgba(30, 125, 207)');
+      if ($(this).scrollTop() > 700){
+        $('#layOn').css('background','rgba(0,0,0,0.7)');
+        // $('body').css('background-image', 'url("images/main.jpg")');
+        // $('body').css('background-attachment', 'fixed');
+        // $('body').css('background-position', '50% 50%');
+        // $('body').css('background-size', 'cover');
+            }
+      if ($(this).scrollTop() < 700) {
+        $('#layOn').css('background','rgba(30, 125, 207)');
       }
    });
 });
